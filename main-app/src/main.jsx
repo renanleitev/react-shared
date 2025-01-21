4562c2ed410f15d57d893fcce5e4988e45ee69b7
Renan Leite Vieira <renanleitev@gmail.com>
2025-01-21 08:54:57 -0300

    feat: Add fluent UI to main app

--- main-app/src/main.jsx
+++ main-app/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
+  </StrictMode>
+);
