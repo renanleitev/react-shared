40e4aee393c52b01f127cea89b992bde54cfcf89
Renan Leite Vieira <renanleitev@gmail.com>
2025-01-21 08:54:19 -0300

    feat: Add fluent UI

--- shared-library/src/components/Button/Button.jsx
+++ shared-library/src/components/Button/Button.jsx
@@ -1,3 +1,6 @@
-export const Button = () => {
-  return <button>Click me</button>;
+/* eslint-disable react/prop-types */
+import { Button } from "@fluentui/react-components";
+
+export const ButtonCTA = (props) => {
+  return <Button>{props.text}</Button>;
 };
